3589ce8759799c064959306b89e4afd81bc16cb8
Kamil Raczycki <raczyckikamil@gmail.com>
2021-10-24 22:21:52 +0200

    Create composite key for PredictionResult

--- db_connector/models/prediction_result.py
+++ db_connector/models/prediction_result.py
@@ -1,11 +1,16 @@
-from peewee import CharField, DecimalField, ForeignKeyField
+from peewee import CharField, CompositeKey, DecimalField, ForeignKeyField
 
 from ._base_model import BaseModel
+from .area import Area
 from .classifier import Classifier
 
 
 class PredictionResult(BaseModel):
-    hex_id = CharField(max_length = 15, primary_key = True, index = True)
-    area = ForeignKeyField(Classifier)
+    hex_id = CharField(max_length = 15, index = True)
+    classifier = ForeignKeyField(Classifier)
+    area = ForeignKeyField(Area)
     value = DecimalField()
+
+    class Meta:
+        primary_key = CompositeKey('hex_id', 'classifier')
     
